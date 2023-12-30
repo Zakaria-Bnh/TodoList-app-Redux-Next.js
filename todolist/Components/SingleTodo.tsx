b1b9b01dea23edf2f89cc33707850a03b08b4bd0
Zakaria-Bnh <zakariabenhanna2002@gmail.com>
2023-12-30 17:55:56 +0100

    the functionality of adding and modifying is finish

--- todolist/Components/SingleTodo.tsx
+++ todolist/Components/SingleTodo.tsx
@@ -1,37 +1,53 @@
-'use client'
+"use client";
 import { Task } from "@/types/AddTaskTypes";
-import { MdModeEdit } from "react-icons/md";
+import { MdModeEdit, MdSignalWifiStatusbarNull } from "react-icons/md";
 import { FaTrashAlt } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa6";
 import { useDispatch } from "react-redux";
 import { deleteTask } from "@/store/slices/AddTaskSlice";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-
- 
-const SingleTodo = ({todo} : {todo: Task}) => {
-  const dispatch = useDispatch()
-  const [isEditing, setIsEditing] = useState(false)
-  const [eidtedText, setEditedText] = useState(todo.title)
+const SingleTodo = ({ todo }: { todo: Task }) => {
+  const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [eidtedText, setEditedText] = useState(todo.title);
+  const taskInput = useRef(null)
 
   const handleDeleteTask = () => {
-    dispatch(deleteTask(todo))
-    console.log('deleted', todo);
-  }
-
-
+    dispatch(deleteTask(todo));
+    console.log("deleted", todo);
+  };
+  const handleEditTask = () => {
+    setIsEditing(!isEditing)   
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.key === 'Enter' && setIsEditing(false)
+    setEditedText(e.target.value)
+    
+  };
+ 
   return (
     <div className="bg-gray-300 text-black p-4 rounded-md shadow-md border border-gray-300">
       <div className="flex justify-between items-center">
-
-        <p>{todo.title}</p>
+        {isEditing ? <input type="text" value={eidtedText} ref={taskInput} onKeyDown={e => handleInputChange(e)} onChange={e => handleInputChange(e)} /> : <p>{eidtedText}</p>}
         <div className="flex gap-4 text-lg">
-          <MdModeEdit className="text-green-400"/>
-          <FaTrashAlt onClick={handleDeleteTask} className="text-red-700 hover:text-red-800 cursor-pointer" />
+          <div onClick={handleEditTask}>
+           { 
+           isEditing ? 
+          <FaCheck className="text-green-400" /> :
+          <MdModeEdit className="text-green-400" /> 
+           }
+          </div>
+          
+          <FaTrashAlt
+            onClick={handleDeleteTask}
+            className="text-red-700 hover:text-red-800 cursor-pointer"
+          />
         </div>
       </div>
-
     </div>
   );
+  
 };
 
 export default SingleTodo;
